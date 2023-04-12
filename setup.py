eda50b1eaf6a01c240de7722f57249509bc162a8
KreshnaaRaam <kreshnaa.raamsb@gmail.com>
2023-04-12 12:56:38 +0530

    README AND setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pypelines',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'json',
+        'sklearn',
+        'pandas'
+    ],
+    entry_points={
+        'console_scripts': [
+            'your_script_name=your_project_name.your_script_module:main',
+        ],
+    },
+    # other metadata
+)
