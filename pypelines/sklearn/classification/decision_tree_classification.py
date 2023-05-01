d08852de28f082dc7e48ebb0496ad0ac7bfcef37
KreshnaaRaam <kreshnaa.raamsb@gmail.com>
2023-05-01 10:58:48 +0530

    adding model types - dt,histgbt,perceptron,sgd

--- pypelines/sklearn/classification/decision_tree_classification.py
+++ pypelines/sklearn/classification/decision_tree_classification.py
@@ -20,14 +20,14 @@ decision_tree_classification_hyperparams = {
 
 class DecisionTreeClassification(SklearnModelBase):
     def __init__(self):
-        model_string = 'DecisionTree()'
+        model_string = 'DecisionTreeClassifier()'
         imports = '''from sklearn.tree import DecisionTreeClassifier\nfrom sklearn.metrics import accuracy_score,make_scorer,f1_score,precision_score,recall_score,roc_auc_score,roc_curve,auc\nimport plotly.express as px'''
         model_type ='Classification'
         super().__init__('dt_classifier', model_string, decision_tree_classification_hyperparams, imports,model_type)
 
 class DecisionTreeClassificationComparison(SklearnModelComparisonBase):
     def __init__(self):
-        model_string = 'DecisionTree()'
+        model_string = 'DecisionTreeClassifier()'
         imports = '''from sklearn.tree import DecisionTreeClassifier\nfrom sklearn.metrics import accuracy_score,make_scorer,f1_score,precision_score,recall_score,roc_auc_score,roc_curve,auc\nimport plotly.express as px'''
         model_type ='Classification'
         super().__init__('dt_classifier', model_string, decision_tree_classification_hyperparams, imports,model_type)
