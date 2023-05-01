76a247b5688745d302055387e4f1b75ffe6c6084
KreshnaaRaam <kreshnaa.raamsb@gmail.com>
2023-05-01 08:01:21 +0530

    docstrings for documentation

--- pypelines/templates/sklearn_model.py
+++ pypelines/templates/sklearn_model.py
@@ -74,6 +74,19 @@ del df, target, features, feature_df, bool_cols, numerical_cols, categorical_col
 
 class SkLearnModelTemplate(AutoPipelineBaseTemplate):
     def __init__(self, requirements=None, required_imports=None, default_values=None):
+        """
+        The __init__ function is used to initialize the class.
+        It takes in a template, requirements, required_imports and default_values as arguments.
+        The template argument is a string that contains the code for your function. 
+        The requirements argument is an optional list of strings that contain any additional packages you need to install before running your function (e.g., ['pandas', 'numpy']). 
+        The required_imports argument is an optional list of strings that contain any additional imports you need to run your function (e.g., ['import pandas as pd', 'import numpy as np
+        
+        :param self: Represent the instance of the class
+        :param requirements: Specify the required packages for this template
+        :param required_imports: Specify the imports that are required for the template to run
+        :param default_values: Set the default values of the parameters that will be used in the template
+        :return: The super() class
+        """
         super().__init__(
             template=template,
             requirements=requirements,
